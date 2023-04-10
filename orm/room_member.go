1a672d7c485ddee66a0337b59feb531e47d00949
Narongrit Mekpho <narongritjamenrtmp@gmail.com>
2023-04-10 13:13:24 +0700

    update room_member

--- orm/room_member.go
+++ orm/room_member.go
@@ -8,5 +8,6 @@ type RoomMember struct {
 	ID string `db:"id" json:"id" gorm:"type:varchar(36);primary_key;"`
 	orm.GormModel
 
+	RoomID string `db:"room_id" json:"room_id" gorm:"type:varchar(36);index;"`
 	UserID string `db:"user_id" json:"user_id" gorm:"type:varchar(36)"`
 }
