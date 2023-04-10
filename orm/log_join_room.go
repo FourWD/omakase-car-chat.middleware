e706b6ab7bfa4a25c57cd8c07fa8e4806c561a89
p-amu <pattanapong.amu@gmail.com>
2023-04-10 14:05:35 +0700

    update birthday type and log join
    type and add orm

---
+++ orm/log_join_room.go
@@ -0,0 +1,16 @@
+package orm
+
+import (
+	orm "github.com/HinekoTech/middleware/orm"
+)
+
+type LogJoinRoom struct { // move to middleware-chat
+	ID string `db:"id" json:"id" gorm:"type:varchar(36);primary_key;"`
+	orm.GormModel
+	UserID       string `db:"user_id" json:"user_id" gorm:"type:varchar(36)"`
+	RoomID       string `db:"room_id" json:"room_id" gorm:"type:varchar(36)"`
+	ActivityType string `db:"activity_type" json:"activity_type" gorm:"type:varchar(3)"`
+	//001 Add member
+	//002 Online
+	//003 Offline
+}
--- orm/user.go
+++ orm/user.go
@@ -1,8 +1,6 @@
 package orm
 
 import (
-	"time"
-
 	orm "github.com/HinekoTech/middleware/orm"
 )
 
@@ -11,13 +9,13 @@ type User struct {
 	orm.GormModel
 	DomainID string `db:"domain_id" json:"domain_id" gorm:"type:varchar(36);"`
 
-	Username  string    `db:"username" json:"username" gorm:"type:varchar(50)"`
-	Password  string    `db:"password" json:"password" gorm:"type:varchar(100)"`
-	Firstname string    `db:"firstname" json:"firstname" gorm:"type:varchar(50)"`
-	Lastname  string    `db:"lastname" json:"lastname" gorm:"type:varchar(50)"`
-	Nickname  string    `db:"nickname" json:"nickname" gorm:"type:varchar(50)"`
-	Birthday  time.Time `db:"birthday" json:"birthday" gorm:"type:datetime"`
-	Avatar    string    `db:"avatar" json:"avatar" gorm:"type:varchar(100)"`
-	Email     string    `db:"email" json:"email" gorm:"type:varchar(50)"`
-	Position  string    `db:"position" json:"position" gorm:"type:varchar(100)"`
+	Username  string `db:"username" json:"username" gorm:"type:varchar(50)"`
+	Password  string `db:"password" json:"password" gorm:"type:varchar(100)"`
+	Firstname string `db:"firstname" json:"firstname" gorm:"type:varchar(50)"`
+	Lastname  string `db:"lastname" json:"lastname" gorm:"type:varchar(50)"`
+	Nickname  string `db:"nickname" json:"nickname" gorm:"type:varchar(50)"`
+	Birthday  string `db:"birthday" json:"birthday" `
+	Avatar    string `db:"avatar" json:"avatar" gorm:"type:varchar(100)"`
+	Email     string `db:"email" json:"email" gorm:"type:varchar(50)"`
+	Position  string `db:"position" json:"position" gorm:"type:varchar(100)"`
 }
