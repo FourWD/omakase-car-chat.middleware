6f4bd01a1fb30348c6cf4cd656fe2b4ca8348e0e
Narongrit Mekpho <dd_narongritjamemii@hotmail.com>
2023-04-11 12:21:09 +0700

    add auth/struct

---
+++ orm/auth.go
@@ -0,0 +1,27 @@
+package orm
+
+import (
+	"time"
+
+	orm "github.com/HinekoTech/middleware/orm"
+)
+
+type LoginBody struct {
+	Username string `db:"username" json:"username" `
+	Password string `db:"password" json:"password" `
+}
+
+type LogiReturn struct {
+	ID string `db:"id" json:"id" gorm:"type:varchar(36);primary_key;"`
+	orm.GormModel
+
+	Username    string    `db:"username" json:"username" `
+	Firstname   string    `db:"firstname" json:"firstname" `
+	Lastname    string    `db:"lastname" json:"lastname" `
+	Nickname    string    `db:"nickname" json:"nickname" `
+	Birthday    time.Time `db:"birthday" json:"birthday" `
+	Avatar      string    `db:"avatar" json:"avatar" `
+	Email       string    `db:"email" json:"email" `
+	Position    string    `db:"position" json:"position" `
+	TokenExpire time.Time `json:"token_expire"`
+}
