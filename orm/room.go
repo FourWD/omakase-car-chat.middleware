a1fb2ac111e995b7801dfbadc87a41c666a9879c
Pattanapong <pattanapong.amu@gmail.com>
2023-04-27 16:54:20 +0700

    update orm room and user

--- orm/room.go
+++ orm/room.go
@@ -5,7 +5,8 @@ import (
 )
 
 type Room struct {
-	ID string `db:"id" json:"id" gorm:"type:varchar(36);primary_key;"`
+	ID         string `db:"id" json:"id" gorm:"type:varchar(36);primary_key;"`
+	FirebaseID string `db:"firebase_id" json:"firebase_id" gorm:"type:varchar(25);"`
 	orm.GormModel
 
 	DomainID string `db:"domain_id" json:"domain_id" gorm:"type:varchar(36);"`
